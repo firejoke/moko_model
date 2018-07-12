6f0d15ded6aa5136fbed720733e4c19ab95bb5ba
fire <firejokeshi@gmail.com>
2018-07-12 10:18:47 +0800

    moko

--- moko_spider.py
+++ moko_spider.py
@@ -5,7 +5,7 @@ import sys
 
 from importlib import import_module
 
-from setting import migrate, URL_DEFAULT
+from setting import migrate, URL_DEFAULT, COOKIES
 
 if __name__ == '__main__':
 	# [print(dir_name) for dir_name in os.listdir('./')]
@@ -16,6 +16,7 @@ if __name__ == '__main__':
 	try:
 		# sys.argv[1].title()
 		model_name = sys.argv[1].title()
+		COOKIES["LAST_LOGIN_EMAIL"] = sys.argv[2]
 		# 模型表映射
 		migrate(model_name)
 		model_path = os.path.join(os.path.dirname(__file__), model_name, 'config')
