e3b7505f19d7885ea15756fc5cf9294d9cf736df
fire <firejokeshi@gmail.com>
2018-07-13 08:57:19 +0800

    moko

--- show_time.py
+++ show_time.py
@@ -12,9 +12,10 @@ from lxml import etree
 # from Model.models import WomanModels
 from setting import HEADERS_DEFAULT, COOKIES, db_session, URL_DEFAULT
 
-new_resp = requests.get(url = 'http://www.moko.cc/post/1217578.html', headers = HEADERS_DEFAULT)
+new_resp = requests.get(url = 'http://www.moko.cc/post/1256601.html', headers = HEADERS_DEFAULT)
 new_html = etree.HTML(new_resp.text)
 create_time = new_html.xpath('//p[@class="date gC1"]/text()')[0]
 title = new_html.xpath('//h2[@class="text dBd_1"]/a/text()')
 title = title if title else new_html.xpath('//h2[@class="text dBd_1"]/a/@title')[0]
-logging.debug(create_time,title)
+photo_list = new_html.xpath('//p[@class="picBox"]//img/@src2')
+logging.debug(create_time,title,photo_list)
